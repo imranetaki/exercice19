9a4516f1bbdc4184a9fbee4c34e3dd918e5abe88
imranetaki <146876470+imranetaki@users.noreply.github.com>
2023-10-08 22:24:36 +0200

    Add files via upload

---
+++ ex19mois.py
@@ -0,0 +1,27 @@
+mois=input("entrer un mois svp : ")
+match mois :
+    case "janvier" :
+          print("31 jours")
+    case "février" : 
+          print("28 ou 29 jours")
+    case "mars"    : 
+          print("31 jours")
+    case "avril"   :
+          print("30 jours")
+    case "mai"     :
+          print("31 jours")
+    case "juin"    :
+          print("30 jours")
+    case "juillet" :
+          print("31 jours")
+    case "aout"    :
+          print("31 jours")
+    case "septembre":
+          print("30 jours")
+    case "octobre" :
+          print("31 jours")
+    case "novembre":
+          print("30 jours")
+    case "decembre":
+          print("31 jours")
+    
